2fc07d0bbbf1e4f0ac03c9bc722048e0f08d097d
Panos Mavrogiorgos <pmav99@gmail.com>
2021-06-03 17:26:21 +0300

    Add tests for netcdf

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -10,7 +10,15 @@ from inspectds.cli import app
 runner = CliRunner()
 
 
-@pytest.mark.parametrize("archive", pathlib.Path("tests/data/").glob("*"))
+@pytest.mark.parametrize("archive", pathlib.Path("tests/data/").glob("*nc*"))
+def test_netcdf(archive: pathlib.Path) -> None:
+    result = runner.invoke(app, ["netcdf", archive.as_posix()])
+    assert result.exit_code == 0
+    assert "Dimensions" in result.stdout
+    assert "attributes" not in result.stdout
+
+
+@pytest.mark.parametrize("archive", pathlib.Path("tests/data/").glob("*zarr*"))
 def test_zarr(archive: pathlib.Path) -> None:
     result = runner.invoke(app, ["zarr", archive.as_posix()])
     assert result.exit_code == 0
